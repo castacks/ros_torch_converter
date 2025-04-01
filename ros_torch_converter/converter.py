a07e7fdc963e8ee69f2843fa967e7063a61b401d
Yifei Liu <amyliu2019@gmail.com>
2025-04-01 12:35:51 -0400

    update datatype for raw 16bit thermal kitti extraction

--- ros_torch_converter/converter.py
+++ ros_torch_converter/converter.py
@@ -5,7 +5,7 @@ from rclpy.node import Node
 
 from ros_torch_converter.datatypes.bev_grid import BEVGridTorch
 from ros_torch_converter.datatypes.float import Float32Torch
-from ros_torch_converter.datatypes.image import ImageTorch, FeatureImageTorch, ThermalImageTorch
+from ros_torch_converter.datatypes.image import ImageTorch, FeatureImageTorch, ThermalImageTorch, Thermal16bitImageTorch
 from ros_torch_converter.datatypes.intrinsics import IntrinsicsTorch
 from ros_torch_converter.datatypes.pointcloud import PointCloudTorch, FeaturePointCloudTorch
 from ros_torch_converter.datatypes.transform import TransformTorch, OdomTransformTorch
@@ -20,6 +20,7 @@ str_to_cvt_class = {
     "Image": ImageTorch,
     "FeatureImage": FeatureImageTorch,
     "ThermalImage": ThermalImageTorch,
+    "Thermal16bitImage": Thermal16bitImageTorch,
     "Intrinsics": IntrinsicsTorch,
     "PointCloud": PointCloudTorch,
     "FeaturePointCloud": FeaturePointCloudTorch,
